1df64746a9be852975247a2d94e74e9c841decdc
Guilherme Brisson <54915600+guiBrisson@users.noreply.github.com>
2023-10-04 17:53:34 -0300

    Core design system module (#7)

---
+++ core/designsystem/src/main/java/me/brisson/revamp/core/module/designsystem/theme/Color.kt
@@ -0,0 +1,13 @@
+package me.brisson.revamp.core.module.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+val darkBackground = Color(0xff242424)
+val darkBackground2 = Color(0xff1C1C1C)
+val darkSurface = Color(0xff2F2F2F)
+val darkPrimary = Color(0xffffffff)
+
+val lightBackground = Color(0xffFFFFFF)
+val lightBackground2 = Color(0xffFFFFFF)
+val lightSurface = Color(0xffF4F4F5)
+val lightPrimary = Color(0xff1C1C1C)
